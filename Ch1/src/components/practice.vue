18121bed7fc712b836adba930f2cde085fc7ff95
mr-aman-git <amanazr2003@gmail.com>
2025-02-23 16:16:55 +0530

    true ok false no

--- Ch1/src/components/practice.vue
+++ Ch1/src/components/practice.vue
@@ -1,23 +1,33 @@
 <template>
     <div>
-        <h1>Message: {{ isTrue ? 'yes' : 'no' }}</h1>
-        <button v-on:click="myFunction">Submit</button>
+        <table>
+            <tr class="border">
+                <td class="border p-4 font-bold">Name</td>
+                <td class="border p-4 font-bold">messsage</td>
+                <td class="border p-4 font-bold">Response</td>
+            </tr>
+
+            <tr class="border" v-for="user in users">
+                <td class="border p-4">{{user.name}}</td>
+                <td class="border p-4">{{user.message}}</td>
+                <td class="border p-4">{{user.response ? 'yes':'no'}}</td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
-export default {
+export default{
     name: 'practice',
-    data() {
-        return {
-            isTrue: true, // Renamed variable
-        };
-    },
-    methods: {
-        myFunction() {
-            console.log('click');
-            this.isTrue = !this.isTrue; // Toggle between true and false
+    data(){
+        return{
+            users:[
+                {name:'Aman', message:'Hello', response:true},
+                {name:'Vishal', message:'Hey', response:true},
+                {name:'Sachin', message:'Good Morning', response:false},
+            ]
         }
     }
-};
+}
+
 </script>
